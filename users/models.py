58ec2df4d63230934ba3b091b70f8bf49e03a94d
jszafran <jszafran.pv@gmail.com>
2019-02-06 23:33:18 +0100

    Implementation of unfollow_user.

--- users/models.py
+++ users/models.py
@@ -7,23 +7,33 @@ class CustomUser(AbstractUser):
 
     def __str__(self):
         return self.email
+    
+    @classmethod
+    def get_user_by_email(cls, email):
+        try:
+            return CustomUser.objects.filter(email=email)[0]
+        except:
+            return None
 
     def get_my_followers(self):
         return [user for user in self.followed_by.all()]
 
     def follow_user(self, user_email):
-        user_to_follow = None
-        try:
-            user_to_follow = CustomUser.objects.filter(email=user_email)[0]
-        except IndexError:
-            print(f"Action failed!\nThere's no user with '{user_email}' email address.")
+        user_to_follow = CustomUser.get_user_by_email(user_email)
         if user_to_follow:
             user_to_follow.followed_by.add(self)
             user_to_follow.save()
             print(f"User '{self.email}' followed user '{user_email}' successfully.")
 
+    def unfollow_user(self, email):
+        user_to_unfollow = CustomUser.get_user_by_email(email=email)
+        if user_to_unfollow:
+            user_to_unfollow.followed_by.remove(self)
+
     def get_users_i_am_following(self):
+        """
+        ???
+        """
         pass
 
-    def unfollow_user(self, user_email):
-        pass
+
