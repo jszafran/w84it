3bbeb9c92824a51c32358c04018a5a66e2b525c2
jszafran <jszafran.pv@gmail.com>
2019-03-20 17:55:27 +0100

    Implementing deletion of product.

--- products/urls.py
+++ products/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 from django.conf.urls import url
-from .views import Product
+from .views import ProductView
 from . import views
 
 urlpatterns = [
-    path('add/', Product.add_product, name='add_product'),
-    url(r'edit/(?P<pk>\d+)/$', Product.edit_product, name='edit_product'),
-
+    path('add/', ProductView.add_product, name='add_product'),
+    url(r'edit/(?P<pk>\d+)/$', ProductView.edit_product, name='edit_product'),
+    url(r'delete/(?P<pk>\d+)/', ProductView.delete_product, name='delete_product'),
 ]
--- products/views.py
+++ products/views.py
@@ -1,12 +1,13 @@
 from django.http import HttpResponse, Http404
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
+from django.urls import reverse
 from django.views.generic import TemplateView
 from .forms import ProductForm
 from .models import Product
 
 
-class Product(TemplateView):
+class ProductView(TemplateView):
     @login_required
     def add_product(request):
         if request.method == 'POST':
@@ -27,16 +28,33 @@ class Product(TemplateView):
         if pk:
             product = get_object_or_404(Product, pk=pk)
             orig_name = product.name
-            if product.owner != request.user:
+            if product.owner != request.user or not product:
                 raise Http404()
 
         if request.method == 'POST':
-            form = ProductForm(request.POST, instance=product, request=request, orig_name=orig_name, action_type='EDIT')
-            if form.is_valid():
-                form.save()
-                return HttpResponse('Article successfully edited!')
-            else:
-                return render(request, 'add_product.html', {'form': form})
+            if 'save_product' in request.POST:
+                form = ProductForm(request.POST, instance=product, request=request, orig_name=orig_name, action_type='EDIT')
+                if form.is_valid():
+                    form.save()
+                    return HttpResponse('Article successfully edited!')
+                else:
+                    return render(request, 'edit_product.html', {'form': form})
+            elif 'delete_product' in request.POST:
+                return redirect('delete_product', pk=pk)
         else:
             form = ProductForm(instance=product)
-            return render(request, 'add_product.html', {'form': form})
+            return render(request, 'edit_product.html', {'form': form})
+
+    @login_required
+    def delete_product(request, pk=None):
+        product = None
+        if pk:
+            product = get_object_or_404(Product, pk=pk)
+            if product.owner != request.user or not product:
+                raise Http404()
+
+        if request.method == 'POST':
+            product.delete()
+            return HttpResponse('Product deleted from database!')
+        else:
+            return render(request, 'confirm_product_deletion.html')
